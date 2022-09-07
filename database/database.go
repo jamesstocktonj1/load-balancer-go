5f59001bddc82efa511988979331ec2f6174a10b
js3g20 <js3g20@soton.ac.uk>
2022-09-07 23:31:24 +0100

    Add Simple API Functions

--- database/database.go
+++ database/database.go
@@ -5,14 +5,58 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	data map[string]string
+)
+
+
 func Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, map[string]string {})
 }
 
 
+func PostData(c *gin.Context) {
+	key := c.Param("key")
+	value := c.Query("value")
+
+	data[key] = value
+	c.JSON(http.StatusOK, map[string]string { "value": data[key] })
+}
+
+func DeleteData(c *gin.Context) {
+	//key := c.Param("key")
+
+}
+
+func GetData(c *gin.Context) {
+	key := c.Param("key")
+
+	c.JSON(http.StatusOK, map[string]string { "value": data[key] })
+}
+
+func PutData(c *gin.Context) {
+	key := c.Param("key")
+	value := c.Query("value")
+
+	data[key] = value
+	c.JSON(http.StatusOK, map[string]string { "value": data[key] })
+}
+
+func DumpData(c *gin.Context) {
+	c.JSON(http.StatusOK, data)
+}
+
+
 func main() {
+
+	data = make(map[string]string)
 
 	router := gin.Default()
 	router.GET("/ping", Ping)
+	router.POST("/data/:key", PostData)
+	router.DELETE("/data/:key", DeleteData)
+	router.GET("/data/:key", GetData)
+	router.PUT("/data/:key", PutData)
+	router.GET("/dump", DumpData)
 	router.Run(":3000")
 }
