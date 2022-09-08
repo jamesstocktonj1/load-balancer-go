b517fd21e7c5c625c60566762160c4159ce779a7
js3g20 <js3g20@soton.ac.uk>
2022-09-08 13:28:48 +0100

    Add Node List and List Ping

--- load-balancer/load-balancer.go
+++ load-balancer/load-balancer.go
@@ -1,33 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"encoding/json"
 	"net/http"
+	"sync"
 	"github.com/gin-gonic/gin"
 )
 
+type node struct {
+	address		string
+	port		string
+	keys		[]string
+}
+
+var nodes = []node {
+	node{ "10.5.0.11", "3000", []string{} },
+	node{ "10.5.0.12", "3000", []string{} },
+}
+
+
+
+func (n node) ContainesKey(key string) bool {
+
+	for _, k := range n.keys {
+
+		if k == key {
+			return true
+		}
+	}
+	return false
+}
+
+func KeyIndex(key string) int {
+
+	for i, n := range nodes {
+
+		if n.ContainesKey(key) {
+			return i
+		}
+	}
+	return -1
+}
+
+
 func Ping(c *gin.Context) {
 
-	resp, err := http.Get("http://10.5.0.11:3000/ping")
-	fmt.Println(resp.Status)
+	var statusArray = map[string]string {}
+	var pingSync sync.WaitGroup
 
-	if err != nil {
-		fmt.Println("Error Pinging Database")
-	} else {
+	for _, n := range nodes {
 
-		defer resp.Body.Close()
-		data, _ := io.ReadAll(resp.Body)
+		pingSync.Add(1)
+		go func(n node) {
 
-		d := map[string]string {}
+			resp, err := http.Get("http://" + n.address + ":" + n.port + "/ping")
 
-		json.Unmarshal(data, &d)
-		
-		fmt.Println(d)
+			if err != nil {
+				statusArray[n.address + ":" + n.port] = "Offline"
+			} else if resp.StatusCode == 200 {
+				statusArray[n.address + ":" + n.port] = "Online"
+			} else {
+				statusArray[n.address + ":" + n.port] = "Error"
+			}
+			pingSync.Done()
+		}(n)
 	}
-	
-	c.JSON(http.StatusOK, map[string]string {})
+	pingSync.Wait()
+
+	c.JSON(http.StatusOK, statusArray)
+}
+
+func DumpData(c *gin.Context) {
+	c.JSON(http.StatusOK, nil)
 }
 
 
